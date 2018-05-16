fa54c0ef579240784608413a5dee911e42ea0149
JavaleWang <21658782@student.uwa.edu.au>
2018-05-17 01:08:05 +0800

    Add unittest

---
+++ unittest/unittest.js
@@ -0,0 +1,25 @@
+
+function EmailErrorMessage({ isFilled, isValid }) {
+  const show = isFilled && !isValid;
+  if (show) {
+    return (
+      1
+    );
+  }
+  return null;
+}
+
+//test EmailErrorMessage with four different inputs
+test('EmailErrorMessage 1',function(){
+	expect(EmailErrorMessage(true,false)).toBe(null);
+});
+test('EmailErrorMessage 2',function(){
+	expect(EmailErrorMessage(true,true)).toBe(null);
+});
+test('EmailErrorMessage 3',function(){
+	expect(EmailErrorMessage(false,false)).toBe(null);
+});
+test('EmailErrorMessage 3',function(){
+	expect(EmailErrorMessage(false,true)).toBe(null);
+});
+//test EmailErrorMessage with four different inputs
